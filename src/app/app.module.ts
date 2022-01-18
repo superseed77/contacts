3dec26a4f7e009ee111a4105e18883dadf2760da
fletcher <bigpanel@gmail.com>
2022-01-18 08:04:32 +0100

    ajout composant list new comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonlistComponent } from './personlist/personlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonlistComponent
   ],
   imports: [
     BrowserModule
