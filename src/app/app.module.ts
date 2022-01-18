2232d1e869e3e866dc68600ddba847bef5ffe00a
yann <bigpanel@gmail.com>
2022-01-18 08:17:37 +0100

    new component detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersondetailComponent } from './persondetail/persondetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersondetailComponent
   ],
   imports: [
     BrowserModule
